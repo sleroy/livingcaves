ea70c6ba52c773a7ea11b17b0d2e79e55051e5c0
Sylvain Leroy <contact@sylvainleroy.com>
2024-09-29 21:33:33 +0200

    fix

--- dye.lua
+++ dye.lua
@@ -1,27 +1,27 @@
 
 minetest.register_craft({
-	output = "dye:orange 2",
+	output = "mcl_dyes:orange 2",
 	recipe = {
 		{"livingcaves:bacteriacave_mold3"}
 	},
 })
 
 minetest.register_craft({
-	output = "dye:violet 2",
+	output = "mcl_dyes:violet 2",
 	recipe = {
 		{"livingcaves:bacteriacave_mold"}
 	},
 })
 
 minetest.register_craft({
-	output = "dye:brown 2",
+	output = "mcl_dyes:brown 2",
 	recipe = {
 		{"livingcaves:rootcave_smallroot"}
 	},
 })
 
 minetest.register_craft({
-	output = "dye:brown 2",
+	output = "mcl_dyes:brown 2",
 	recipe = {
 		{"livingcaves:rootcave_smallroot2"}
 	},
@@ -29,14 +29,14 @@ minetest.register_craft({
 
 
 minetest.register_craft({
-	output = "dye:green 2",
+	output = "mcl_dyes:green 2",
 	recipe = {
 		{"livingcaves:hangingmoss"}
 	},
 })
 
 minetest.register_craft({
-	output = "dye:green 2",
+	output = "mcl_dyes:green 2",
 	recipe = {
 		{"livingcaves:hangingmossend"}
 	},
@@ -44,35 +44,35 @@ minetest.register_craft({
 
 
 minetest.register_craft({
-	output = "dye:grey 2",
+	output = "mcl_dyes:grey 2",
 	recipe = {
 		{"livingcaves:bacteriacave_mold2"}
 	},
 })
 
 minetest.register_craft({
-	output = "dye:grey 2",
+	output = "mcl_dyes:grey 2",
 	recipe = {
 		{"livingcaves:hangingmold"}
 	},
 })
 
 minetest.register_craft({
-	output = "dye:grey 2",
+	output = "mcl_dyes:grey 2",
 	recipe = {
 		{"livingcaves:hangingmoldend"}
 	},
 })
 
 minetest.register_craft({
-	output = "dye:dark_green 2",
+	output = "mcl_dyes:dark_green 2",
 	recipe = {
 		{"livingcaves:moss"}
 	},
 })
 
 minetest.register_craft({
-	output = "dye:cyan 2",
+	output = "mcl_dyes:cyan 2",
 	recipe = {
 		{"livingcaves:lichen"}
 	},
